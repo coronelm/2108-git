0b9af3dced3c15e001beef8b01e03535075e0a69
PrincessBalancio <24-30682@g.batstate-u.edu.ph>
2025-11-06 14:44:40 +0800

    GROUP 7 CREATED SUBJECT TABLE

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VAR
 Create table teachers (id int auto_increment primary key, name varchar(100), subject varchar(100))
 insert into teachers (id, name, subject) values (001, 'Gian', 'DBMS');
 CREATE TABLE CLASSES (class_id int AUTO_INCREMENT PRIMARY KEY, class_name VARCHAR(50), room_num int);
+CREATE TABLE Subjects (subject_id int PRIMARY KEY, subject_name VARCHAR);

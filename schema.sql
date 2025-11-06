236d4b2c8c37f583f175d1c55e2f51b444377d44
Solo-2414 <danielrodge.caindoy@gmail.com>
2025-11-06 14:05:11 +0800

    Group 3 Created Classes Table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;
 USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
 Create table teachers (id int auto_increment primary key, name varchar(100), subject varchar(100))
 insert into teachers (id, name, subject) values (001, 'Gian', 'DBMS');
+CREATE TABLE CLASSES (class_id int AUTO_INCREMENT PRIMARY KEY, class_name VARCHAR(50), room_num int);

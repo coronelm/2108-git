58c404be0ef1f5a34aaf71ea789c7b61bdab9fbc
shibyyyy <mendozaraine0@gmail.com>
2025-11-06 13:45:18 +0800

    Group 1 created teachers table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db;
 USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
+Create table teachers (id int auto_increment primary key, name varchar(100), subject varchar(100))
